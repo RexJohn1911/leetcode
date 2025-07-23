94dcca25e468b535445e8e7cefdb5a29a40bda7c
RexJohn1911 <rexjohnabraham.28csb@licet.ac.in>
2025-07-23 21:21:57 +0530

    Time: 1315 ms (11.41%) | Memory: 24.8 MB (30.70%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.c
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.c
@@ -2,21 +2,21 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* sortedSquares(int* nums, int numsSize, int* returnSize) {
-    *returnSize=numsSize;
-    int *ptr=(int*)malloc(numsSize*(sizeof(int)));
-    int i,j,temp;
-    for(i=0;i<numsSize;i++){
-        ptr[i]=nums[i]*nums[i];
-        ptr[j]=nums[j]*nums[j];
-    }
-    for(i=0;i<numsSize-1;i++){
-        for(j=i+1;j<numsSize;j++){
-            if(ptr[i]>ptr[j]){
-                temp=ptr[i];
-                ptr[i]=ptr[j];
-                ptr[j]=temp;
-            }
+*returnSize=numsSize;
+int *ptr=(int*)malloc(numsSize*(sizeof(int)));
+int i,j,temp;
+for(i=0;i<numsSize;i++){
+    ptr[i]=nums[i]*nums[i];
+    ptr[j]=nums[j]*nums[j];
+}
+for(i=0;i<numsSize-1;i++){
+    for(j=i+1;j<numsSize;j++){
+        if(ptr[i]>ptr[j]){
+            temp=ptr[i];
+            ptr[i]=ptr[j];
+            ptr[j]=temp;
         }
     }
-    return ptr;
+}
+return ptr;
 }
