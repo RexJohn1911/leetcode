a8e1bd4897294fda371c36f2d21632b6285b10f4
RexJohn1911 <rexjohnabraham.28csb@licet.ac.in>
2025-09-13 10:03:23 +0530

    Time: 6 ms (65.31%) | Memory: 45.6 MB (45.15%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean doesAliceWin(String s) {
+        for(char c:s.toCharArray()){
+            if(isvowel(c))return true;
+        }
+        return false;
+    }
+    private boolean isvowel(char c){
+        return c=='a' || c=='e' || c=='i' || c=='o' || c=='u';
+    }
+}
