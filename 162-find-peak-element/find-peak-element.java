e5c435fa07f4803e685733757750ca79f154420f
RexJohn1911 <rexjohnabraham.28csb@licet.ac.in>
2025-10-09 23:23:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (86.92%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int l=0;
+        int h=nums.length-1;
+        while(l<h){
+            int mid=(l+h)/2;
+            if(nums[mid]>nums[mid+1]){
+                h=mid;
+            }
+            if(nums[mid]<nums[mid+1]){
+                l=mid+1;
+            }
+        }
+        return l;
+    }
+}
